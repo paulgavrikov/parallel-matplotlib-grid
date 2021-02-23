a4fabe8b438246ef361e3320a777dce9d0419c13
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2021-02-23 22:05:57 +0100

    Addign setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="parallelplot",
+    version="0.0.1",
+    author="Paul Gavrikov",
+    author_email="paul.gavrikov@hs-offenburg.de",
+    description="Parallel plotting of matplotlib subplots.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paulgavrikov/parallel-matplolib-grid",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'matplotlib'
+    ],
+    python_requires='>=3.6',
+)
