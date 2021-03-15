ba4eda464fe4b2a976408b8932e10165c91fbf52
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2021-03-15 16:02:39 +0100

    Adding in_memory images communication
    Instead of saving figs to drive, we can store them in a buffer, convert it to a PIL images and communicate it back. This is slightly faster. Enabled by default.
    
    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="parallelplot",
-    version="0.0.1",
+    version="0.0.2",
     author="Paul Gavrikov",
     author_email="paul.gavrikov@hs-offenburg.de",
     description="Parallel plotting of matplotlib subplots.",
@@ -19,7 +19,8 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'matplotlib'
+        'matplotlib',
+        'pillow'
     ],
     python_requires='>=3.6',
 )
